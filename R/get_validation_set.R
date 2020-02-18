2d1f0d4d363ba23a78414aea4130d8b4adb3cc47
AnthonyTedde <antho.tedde@gmail.com>
2020-02-18 10:53:50 +0100

    create get_validation_set

---
+++ R/get_validation_set.R
@@ -0,0 +1,84 @@
+#' Get validation set
+#'
+#' Creation of validation sets based on some criterions, such as the proportion
+#' of data going to the validation set `bound` ans
+#'
+#' @param data Data.frame. Candidate data.frame.
+#' @param variables Character. What variables to consider in PCA.
+#' @param bound Integer. Lower and upper boundary of data proportion to
+#' be covered by the validation set.
+#' @param group Character. Pivot variable which discriminate each group.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_validation_set <- function(data,
+                               variables = dplyr::everything(),
+                               bound = c(0.10, 0.20),
+                               group){
+  lower <- min(bound)
+  upper <- max(bound)
+
+  data_prop <- data %>%
+    dplyr::count(!!!dplyr::syms(group)) %>%
+    dplyr::mutate(prop = n/sum(n))
+
+  groupset_combination_lst <- seq_along(data_prop[[group]]) %>%
+    purrr::map(combn, x = data_prop[[group]], simplify = F) %>%
+    purrr::reduce(c)
+
+  sub_data_lst <- groupset_combination_lst %>%
+    purrr::map(~dplyr::filter(data_prop, data_prop[[group]] %in% .x))
+
+  subset_lgl <- sub_data_lst %>%
+    purrr::map(dplyr::pull, prop) %>%
+    purrr::map(sum) %>%
+    purrr::map_lgl(~ (.x > lower & .x < upper))
+
+  subset_final <- sub_data_lst[subset_lgl] %>%
+    purrr::map(dplyr::pull, group)
+
+  scores_lst <- subset_final %>%
+    purrr::map(.f = function(x){
+
+      ###################################
+      # data split
+      ###################################
+      data_split_lst <- data %>%
+        dplyr::mutate(pivot = expl_id %in% x) %>%
+        dplyr::group_by(pivot) %>%
+        dplyr::group_split() %>%
+        purrr::map(dplyr::select, variables) %>%
+        setNames(nm = c("train", "test"))
+
+      # Just in case...
+      if(length(data_split_lst$train) < length(data_split_lst$test))
+        names(data_split_lst) <- rev(names(data_split_lst))
+
+      ###################################
+      # PCA on train
+      ###################################
+
+      data_train_pca_preproc <- data_split_lst$train %>%
+        caret::preProcess(method = c("center", "scale", "nzv", "pca"))
+
+      data_train_pca <- predict(data_train_pca_preproc, data_split_lst$train) %>%
+        tibble::as_tibble()
+
+      data_test_pca <- predict(data_train_pca_preproc, data_split_lst$test) %>%
+        tibble::as_tibble()
+
+      pc_test_mahalanobis <- data_test_pca %>%
+        mahalanobis(., center = colMeans(data_train_pca), cov = cov(.))
+
+      ### Standardization
+      pc_test_mahalanobis / ncol(data_test_pca)
+
+    })
+
+  return(list(
+    subsets = subset_final,
+    score = scores_lst
+  ))
+}
